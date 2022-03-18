8770d5c951e9ecf1c8598673b17d6a1f1997baed
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-18 15:57:52 -0500

    Add omniauth mocking

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,3 +78,6 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/support/vcr_cassettes"
   config.hook_into :webmock
 end
+
+OmniAuth.config.test_mode = true
+OmniAuth.config.mock_auth[:google_oauth2] = Faker::Omniauth.google
