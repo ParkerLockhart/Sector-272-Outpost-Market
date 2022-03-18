248fcc16ef944331a629648b45a3bc38dd0de911
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-18 16:09:26 -0500

    Google oauth registration and testing

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -6,16 +6,24 @@ RSpec.describe 'user registration page' do
   end
 
   it 'has a form to create a new user' do
-    fill_in 'user_username', with: "Jeff33"
-    fill_in 'user_password', with: "password"
-    fill_in 'user_password_confirmation', with: "password"
-    click_button("Register")
+    fill_in 'user_username', with: 'Jeff33'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+    click_button('Register')
     user = User.last
 
-    expect(current_path).to eq("/dashboard")
+    expect(current_path).to eq('/dashboard')
+    expect(user.username).to eq('Jeff33')
   end
 
   it 'links to google registration' do
     expect(page).to have_link('Register with Google')
+  end
+
+  it 'can create a user with google_oauth2/omniauth' do
+    visit '/auth/google_oauth2/callback'
+    user = User.last
+
+    expect(current_path).to eq('/dashboard')
   end
 end
