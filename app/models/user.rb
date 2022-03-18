457a652bafaa0e018eca8b34b2f379dfdd1716a1
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-17 21:44:11 -0500

    Add user secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: true
   enum role: [:default, :manager, :admin]
+
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username) }
   end
+
+  describe 'secure user creation' do
+    it 'saves user auth info securely' do
+      user = User.create(username: "Jeff33", password: "password", password_confirmation: "password")
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq("password")
+    end
+  end 
 end
