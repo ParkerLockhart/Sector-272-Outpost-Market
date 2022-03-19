30dd285f4dddef6fde0953fc799f070722d9a34f
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-18 20:20:25 -0500

    merchant show test user auth refactor, holiday service faraday refactor

--- app/services/holiday_service.rb
+++ app/services/holiday_service.rb
@@ -1,10 +1,7 @@
-require 'httparty'
-require 'json'
-
 class HolidayService
 
   def next_holidays
-    response = HTTParty.get("https://date.nager.at/api/v2/NextPublicHolidays/us")
+    response = Faraday.get("https://date.nager.at/api/v2/NextPublicHolidays/us")
     parsed = JSON.parse(response.body, symbolize_names: true)
   end
 
