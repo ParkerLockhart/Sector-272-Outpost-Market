9a8771a1d72945ab0a2a4c8fe23926995bd461b0
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-16 20:03:00 -0500

    Update factories

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     association :invoice, factory: :invoice
     association :item, factory: :item
     quantity { Faker::Number.number(digits: 2) }
-    unit_price { Faker::Number.number(digits: 4) }
+    calculated_price { Faker::Number.number(digits: 4) }
     status { ['packaged', 'pending', 'shipped'].sample }
   end
 end
--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -2,6 +2,8 @@ require 'faker'
 FactoryBot.define do
   factory :invoice do
     association :customer, factory: :customer
+    association :merchant, factory: :merchant
     status { [0, 1, 2].sample }
+    total { Faker::Number.number(digits: 4) }
   end
 end
